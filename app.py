55635948eb4b2af44b99669fda5cca59b8ce36e3
RohanOpenSource <dude123code@gmail.com>
2022-06-07 11:59:38 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send 
+
+app = Flask(__name__)
+
+app.config['SECRET'] = 'secret'
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@socketio.on('message')
+def handle_message(message):
+    print(message)
+    send(message, broadcast=True)
+
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="localhost")
